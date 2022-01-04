d2a140f942c4103b9bee1c1e6af5fca27534a3a0
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-03 23:35:20 -0300

    Others config

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true
+    })
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Book Store')
+    .setDescription('API de uma loja de livros')
+    .addTag('User')
+    .addTag('Book')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
