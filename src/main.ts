6a6f6f48552fc9c8ba149d29dbdec22b60de0f56
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-04 22:29:15 -0300

    Cleanup project

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -22,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
